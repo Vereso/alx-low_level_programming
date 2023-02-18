c7457081bb3c9031ddb7dd7f535e3bbcb0707af9
Vereso <veresosally@gmail.com>
2023-02-18 07:19:00 -0800

    Corrected Firts code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* An program that shows whether an integer is positive or negative */
+/* Main: A  program that shows whether an integer is positive or negative
+ * Description: using the main function */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -11,15 +12,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%i is positive\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-	else (n<o)
+	else if (n < 0)
 	{
 		printf("%i is negative\n", n);
 	}
