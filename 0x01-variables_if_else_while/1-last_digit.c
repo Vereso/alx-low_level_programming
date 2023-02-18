9a04e9665fecb4e0452531e71e2546a1c00a9741
Vereso <veresosally@gmail.com>
2023-02-18 08:37:21 -0800

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,10 @@
 /* Main- checks the last digit of the number*/
 
 /**
- * %10 determines the last digit of the number
  * relational operators determine whether number is 0, >5, or 5
+ * success Return: 0
  */
+
 int main(void)
 {
 	int n;
@@ -14,8 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	int last_digit;
 	last_digit = n % 10;
-	if (last_digit > 5);
+	if (last_digit > 5)
 	{
 		printf("Last digit of %i is %d and is greater than 5\n", n, last_digit);
 	}
@@ -23,7 +25,7 @@ int main(void)
 	{
 		printf("Last digit of %i is %d and is zero\n", n, last_digit);
 	}
-	else if (last_digit < 6 && != 0)
+	else if (last_digit != 0 && < 6)
 	{
 		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_digit);
 	}
