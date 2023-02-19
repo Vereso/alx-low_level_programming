25295ecb33f8338c108b8a5e1f0d66a7ae5aee0a
Vereso <veresosally@gmail.com>
2023-02-19 08:05:34 -0800

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/** 
+ * main: prints all possible combinations of 0-9
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
