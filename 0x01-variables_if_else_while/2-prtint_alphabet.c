ec93f151a4dbe575d51b9b52435c30b816b6ec7b
Vereso <veresosally@gmail.com>
2023-02-18 19:39:10 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-prtint_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * return: Always zero
+ */
+
+int main(void)
+{
+	for(char letter = 'a', letter <= z, letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
