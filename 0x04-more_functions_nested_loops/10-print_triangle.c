ef1acd92bb58e5bbf615f8ef87e74e9042207197
Vereso <veresosally@gmail.com>
2023-02-24 18:58:41 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	
+    	if (size <= 0) 
+	{
+        _putchar('\n');
+        return;
+	}
+	for (i = 1; i <= size; i++) 
+	{
+        for (j = 1; j <= i; j++) 
+	{
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
+
+
