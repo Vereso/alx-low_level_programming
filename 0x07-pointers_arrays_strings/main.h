cdd3e716a32ad2b38138528830a27bbcf5fd6dc3
Vereso <veresosally@gmail.com>
2023-03-06 17:51:18 -0800

    Diagonal sums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stddef.h>
 
+void print_diagsums(int *a, int size);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
