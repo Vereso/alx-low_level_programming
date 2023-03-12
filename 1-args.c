b90f261c8515eb229c26efede4583341e826a963
Vereso <veresosally@gmail.com>
2023-03-12 01:28:11 -0800

    Number of arguments passed to it

---
+++ 1-args.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc);
+	return (0);
+}
+
