3be509f615edc9d7fa2fcb76f9efd2bab8348ff6
Vereso <veresosally@gmail.com>
2023-06-08 16:12:43 +0300

    Function that prints a string

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: ALways 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with Recursion");
+	return(0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#define MAIN_H
+#ifndef MAIN_H
+#include <stdio.h>
+#include "main.h"
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
